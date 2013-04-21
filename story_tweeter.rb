862227eaf9edca92f4e0b510dafbdb485175609b
Donald McKendrick <ddmckendrick@gmail.com>
2013-04-21 18:58:26 +0200

    Tidys code, removes counters. Issues with API.

--- story_tweeter.rb
+++ story_tweeter.rb
@@ -7,6 +7,7 @@ Twitter.configure do |config|
   config.oauth_token_secret = "YOUR_OAUTH_SECRET"
 end
 
+# Finds a place to stop that isn't mid-word
 def logical_stop(cut)
   until cut[-1] == " "
     cut = cut[0..-2]
@@ -14,7 +15,8 @@ def logical_stop(cut)
   return cut
 end
 
-def chunker(tweet, num)
+# Splits the text into 
+def chunker(tweet)
   sample = logical_stop(tweet[0,135])
   return sample, tweet[sample.length..-1]
 end   
@@ -23,20 +25,18 @@ def recurring(tweet)
   tweets = Hash.new
   n = 1
   until tweet.length < 135
-    sample, tweet = chunker(tweet, n)
+    sample, tweet = chunker(tweet)
     tweets[n] = sample
     n += 1
   end
   return tweet, n, tweets
 end
 
-def multi_tweet(array, n)
-  part = 1
-  until part > n
-    Twitter.update(array[part] + "(#{part}/#{n})")
+def multi_tweet(hash, n)
+  (1..n).each do |part|
+    Twitter.update(hash[part] + "(#{part}/#{n})")
     # For Testing without submitting to twitter
-    #puts (array[part] + "(#{part}/#{n})")
-    part += 1
+    #puts (hash[part] + "(#{part}/#{n})")
   end
 end
 
